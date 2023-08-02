3ffa4884958c21f0c32c125015f5971a9f1da5dc
go-to-stars <121138297+go-to-stars@users.noreply.github.com>
2023-08-02 23:29:40 +0000

    Deploying to gh-pages from @ go-to-stars/goit-react-hw-05-movies@a3d7832c9b4912150b71907788d2d5eed15989a3 🚀

---
+++ static/js/692.10812315.chunk.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunkgoit_react_hw_05_movies=self.webpackChunkgoit_react_hw_05_movies||[]).push([[692],{7692:function(t,e,i){i.d(e,{YiX:function(){return n}});var a=i(9983);function n(t){return(0,a.w_)({tag:"svg",attr:{viewBox:"0 0 24 24"},child:[{tag:"path",attr:{d:"M21 11H6.414l5.293-5.293-1.414-1.414L2.586 12l7.707 7.707 1.414-1.414L6.414 13H21z"}}]})(t)}}}]);
+//# sourceMappingURL=692.10812315.chunk.js.map
